f79e99883ee1ca6ef251cd5e90e49a42ebd41005
myrna90 <myrnamares90@gmail.com>
2020-09-22 18:06:50 -0500

    Updates

---
+++ precache-manifest.727a617818987c6941075ada4715f373.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a103808339b4918daf4460c64e703e77",
+    "url": "/punto-commerce-myrna-mares/index.html"
+  },
+  {
+    "revision": "1ee2e344d98b0a4884f6",
+    "url": "/punto-commerce-myrna-mares/static/css/main.83ea7077.chunk.css"
+  },
+  {
+    "revision": "4fccd6a174f86ad238fd",
+    "url": "/punto-commerce-myrna-mares/static/js/2.62e39efe.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/punto-commerce-myrna-mares/static/js/2.62e39efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee2e344d98b0a4884f6",
+    "url": "/punto-commerce-myrna-mares/static/js/main.958eb44d.chunk.js"
+  },
+  {
+    "revision": "a5f48a5c5b77cb98b67d",
+    "url": "/punto-commerce-myrna-mares/static/js/runtime-main.8245086f.js"
+  },
+  {
+    "revision": "2f0b48ba3f035d81516fc6a9b61b9d4a",
+    "url": "/punto-commerce-myrna-mares/static/media/1.2f0b48ba.jpg"
+  },
+  {
+    "revision": "bd6af6918ba03e3b435a2ef68dd10151",
+    "url": "/punto-commerce-myrna-mares/static/media/3.bd6af691.jpg"
+  },
+  {
+    "revision": "98f0a861ca6bb48f5906dfbc467bf14c",
+    "url": "/punto-commerce-myrna-mares/static/media/4.98f0a861.jpg"
+  },
+  {
+    "revision": "f8c448fed9dd978063d90c0c6f8913e8",
+    "url": "/punto-commerce-myrna-mares/static/media/5.f8c448fe.jpg"
+  },
+  {
+    "revision": "31a364265362fe840c4e1dfa69c58699",
+    "url": "/punto-commerce-myrna-mares/static/media/6.31a36426.jpg"
+  },
+  {
+    "revision": "406fc0c5e7e19fe325196b2d2c76d79a",
+    "url": "/punto-commerce-myrna-mares/static/media/Rectangle1.406fc0c5.png"
+  },
+  {
+    "revision": "b888b3831cd68b6db0d974e30b99b4bb",
+    "url": "/punto-commerce-myrna-mares/static/media/Rectangle2.b888b383.png"
+  },
+  {
+    "revision": "9df8fdc552bd2d596c3b68092a334131",
+    "url": "/punto-commerce-myrna-mares/static/media/Rectangle3.9df8fdc5.png"
+  }
+]);
